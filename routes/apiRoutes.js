6f8160a67d8bc4df4b77039c8f76082f67df2d0c
Sam Ayler <sayler8701@gmail.com>
2021-02-22 13:42:11 -0800

    adding post route WORKING

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,4 +11,15 @@ module.exports = (router) => {
 			.sort({ date: -1 })
 			.then((employees) => res.json(employees));
 	});
+
+	//post new employee
+	router.post("/api/employees", (req, res) => {
+		Employee.create(req.body)
+			.then((data) => {
+				res.json(data);
+			})
+			.catch((err) => {
+				res.status(400).json(err);
+			});
+	});
 };
