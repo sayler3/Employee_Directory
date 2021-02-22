8f34c5708e84dab908b088b29835a422f31f9d58
Sam Ayler <sayler8701@gmail.com>
2021-02-22 10:58:35 -0800

    update routes for get all WORKING

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+//employee model
+const Employee = require("../models/employee");
+
 module.exports = (router) => {
-	router.get("/backend", (req, res) => {
-		res.send("Hello from the backend");
+	//get all employees
+	router.get("/api/employees", (req, res) => {
+		Employee.find({})
+			.sort({ date: -1 })
+			.then((employees) => res.json(employees));
 	});
 };
