c10dc8bde7f1fceb2552a6fe8a23b6a9d4dfd85d
Sam Ayler <sayler8701@gmail.com>
2021-02-23 14:37:38 -0800

    updating app.js with browser router and route for all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import Home from "./Pages/Home";
+import Search from "./Pages/Search";
+import Add from "./Pages/Add";
+
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World!</h1>
+			<BrowserRouter>
+				<NavBar />
+				<Switch>
+					<Route path="/search" component={Search} />
+					<Route path="/add" component={Add} />
+					<Route path="/" component={Home} />
+				</Switch>
+			</BrowserRouter>
 		</div>
 	);
 }
