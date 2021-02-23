98a7052b2c435c1a2825e317f8e77d2260607688
Sam Ayler <sayler8701@gmail.com>
2021-02-23 07:57:40 -0800

    update for seeds data all WORKING

--- models/employee.js
+++ models/employee.js
@@ -3,51 +3,12 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const employeeSchema = new Schema({
-	fullName: [
-		{
-			first: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-			last: {
-				type: String,
-				trim: true,
-				required: true,
-			},
-		},
-	],
-	location: [
-		{
-			street: {
-				type: String,
-				trim: true,
-			},
-			city: {
-				type: String,
-				trim: true,
-			},
-			state: {
-				type: String,
-				trim: true,
-			},
-			postcode: {
-				type: String,
-				trim: true,
-			},
-		},
-	],
-	email: {
-		type: String,
-		trim: true,
-	},
-	cell: {
-		type: Number,
-		trim: true,
-	},
 	date: {
 		type: Date,
 		default: Date.now,
+	},
+	employee: {
+		type: Array,
 	},
 });
 
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,13 +1,49 @@
 // Setting up to seed database with randomuser api
 const axios = require("axios");
+const mongoose = require("mongoose");
+// const Employee = require("../models/employee");
 
-axios
-	.get("https://randomuser.me/api/?results=2")
-	.then(function (response) {
-		// handle success
-		console.log(response.data);
+//employee model
+const Employee = require("../models/employee");
+
+let employeeSeeds = [];
+
+// setting up database connection for deployment and localhost
+mongoose
+	.connect("mongodb://localhost/directory", {
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
 	})
-	.catch(function (error) {
-		// handle error
-		console.log(error);
-	});
+	.then(() => console.log("MongoDB connected..."))
+	.catch((err) => console.log(err));
+
+const seedDB = async () => {
+	axios
+		.get(
+			"https://randomuser.me/api/?results=20&nat=us&exc=gender,registered,nat,id,phone,login"
+		)
+		.then(function (response) {
+			// handle success
+			employeeSeeds = response.data.results;
+			// newData.map((items) =>
+			// 	items.filter((employee) => employee === employee.name)
+			// );
+			// newData.filter((employee) => employee === employee.name);
+			// console.log(newData[0].name.first);
+			// console.log(employeeSeeds);
+			// console.log(Object.keys(newData));
+		})
+		.then(() => Employee.deleteMany({}))
+		.then(() => Employee.collection.insertMany(employeeSeeds))
+		.then((data) => {
+			console.log(data.result.n + " records inserted!");
+			process.exit(0);
+		})
+		.catch((err) => {
+			console.error(err);
+			process.exit(1);
+		});
+};
+seedDB();
