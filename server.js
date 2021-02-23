22165bc926d041dc656fdde93cac5a588515e1bf
Sam Ayler <sayler8701@gmail.com>
2021-02-22 19:29:32 -0800

    setting scripts to run concurrently and changing port number for backend

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 // setting port for deployment and running localhost
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // setting up database connection for deployment and localhost
 mongoose
