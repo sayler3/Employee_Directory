030f1361493adaeda8257a7fed752b2f00d12360
Sam Ayler <sayler8701@gmail.com>
2021-02-23 18:13:25 -0800

    connect backend to frontend

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Cards from "../Components/Cards";
+import axios from "axios";
 
 const Home = () => {
+	let response;
+	const [employeeData, setemployeeData] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:5000/api/employees")
+			.then((res) => {
+				setemployeeData(res);
+				console.log(res);
+				console.log(employeeData);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
 	return (
 		<div className="container">
-			<h1>Hello from home</h1>
+			<h1>All Employees</h1>
 			<Cards src="https://randomuser.me/api/portraits/women/84.jpg" />
 		</div>
 	);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //bring in dependencies
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -24,6 +25,7 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 // bringing in routes
 require("./routes/apiRoutes")(app);
