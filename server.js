6c87b75292b7aee72b308c95d6b3599089e8b0f9
Sam Ayler <sayler8701@gmail.com>
2021-02-26 09:30:23 -0800

    updating server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
 
 // setting up middleware
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static(path.join(__dirname, "client", "build")));
 
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
