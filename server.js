121c878883c9a969be9ab32aba17c1b88ca2c0dc
Sam Ayler <sayler8701@gmail.com>
2021-02-26 09:50:02 -0800

    reverting

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ mongoose
 
 // setting up middleware
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client", "build")));
+	app.use(express.static("client/build"));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
 }
 
