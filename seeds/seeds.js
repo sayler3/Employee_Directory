4a5dbd0aa121d8d652037ec781581c41730d8741
Sam Ayler <sayler8701@gmail.com>
2021-02-25 23:15:22 -0800

    updating seeds file with comments

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,124 +1,85 @@
 // Setting up to seed database with randomuser api
 const axios = require("axios");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const Employee = require("../models/employee");
 
 //employee model
-// const Employee = require("../models/employee");
+const Employee = require("../models/employee");
 
-// let employeeSeeds = [];
+let employeeSeeds = [];
 
-// // setting up database connection for deployment and localhost
-// mongoose
-// 	.connect("mongodb://localhost/directory", {
-// 		useNewUrlParser: true,
-// 		useFindAndModify: false,
-// 		useCreateIndex: true,
-// 		useUnifiedTopology: true,
-// 	})
-// 	.then(() => console.log("MongoDB connected..."))
-// 	.catch((err) => console.log(err));
+// setting up database connection for deployment and localhost
+mongoose
+	.connect("mongodb://localhost/directory", {
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("MongoDB connected..."))
+	.catch((err) => console.log(err));
 
-// const seedDB = async () => {
-// 	axios
-// 		.get(
-// 			"https://randomuser.me/api/?results=20&nat=us&exc=gender,registered,nat,id,phone,login"
-// 		)
-// 		.then(function (response) {
-// 			// handle success
-// 			employeeSeeds = response.data.results;
-// 			// newData.map((items) =>
-// 			// 	items.filter((employee) => employee === employee.name)
-// 			// );
-// 			// newData.filter((employee) => employee === employee.name);
-// 			// console.log(newData[0].name.first);
-// 			// console.log(employeeSeeds);
-// 			// console.log(Object.keys(newData));
-// 		})
-// 		.then(() => Employee.deleteMany({}))
-// 		.then(() => Employee.collection.insertMany(employeeSeeds))
-// 		.then((data) => {
-// 			console.log(data.result.n + " records inserted!");
-// 			process.exit(0);
-// 		})
-// 		.catch((err) => {
-// 			console.error(err);
-// 			process.exit(1);
-// 		});
-// };
-// seedDB();
+const seedDB = async () => {
+	axios
+		.get(
+			"https://randomuser.me/api/?results=20&nat=us&exc=gender,registered,nat,id,phone,login"
+		)
+		.then(function (response) {
+			// handle success
+			employeeSeeds = response.data.results;
+		})
+		.then(() => Employee.deleteMany({}))
+		.then(() => Employee.collection.insertMany(employeeSeeds))
+		.then((data) => {
+			console.log(data.result.n + " records inserted!");
+			process.exit(0);
+		})
+		.catch((err) => {
+			console.error(err);
+			process.exit(1);
+		});
+};
+seedDB();
+
+//------------------------------------------------------------------------------
+// To seed mongo Atlas run code commented code below
 
 // const MongoClient = require("mongodb").MongoClient;
-// const uri =
-// 	"mongodb+srv://sam-admin:AhPbHcDtN25KzQcb@cluster0.fdhzm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, {
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true,
-// });
-// client.connect((err) => {
-// 	client.db("directory").collection("employees");
-// 	// perform actions on the collection object
-// 	let employeeSeeds = [];
 
-// 	axios
-// 		.get(
-// 			"https://randomuser.me/api/?results=50&nat=us&exc=gender,registered,nat,id,phone,login"
-// 		)
-// 		.then(function (response) {
-// 			// handle success
-// 			employeeSeeds = response.data.results;
-// 		})
-// 		.then(() => collection.drop())
-// 		.then(() => collection.insertMany(employeeSeeds))
-// 		.then((data) => {
-// 			console.log(data.result.n + " records inserted!");
-// 		})
-// 		.catch((err) => {
-// 			console.error(err);
-// 		});
-// 	client.close();
-// });
+// async function seedDB() {
+// 	// Connection URL
+// 	const uri = "";
 
-/* mySeedScript.js */
+// 	const client = new MongoClient(uri, {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	});
 
-// require the necessary libraries
-const MongoClient = require("mongodb").MongoClient;
+// 	try {
+// 		await client.connect();
+// 		console.log("Connected correctly to server");
 
-async function seedDB() {
-	// Connection URL
-	const uri =
-		"mongodb+srv://sam-admin:AhPbHcDtN25KzQcb@cluster0.fdhzm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// 		const collection = client.db("directory").collection("employees");
 
-	const client = new MongoClient(uri, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
+// 		collection.drop();
 
-	try {
-		await client.connect();
-		console.log("Connected correctly to server");
+// 		let employeeSeeds = [];
 
-		const collection = client.db("directory").collection("employees");
+// 		await axios
+// 			.get(
+// 				"https://randomuser.me/api/?results=50&nat=us&exc=gender,registered,nat,id,phone,login"
+// 			)
+// 			.then(function (response) {
+// 				// handle success
+// 				employeeSeeds = response.data.results;
+// 			})
+// 			.then(() => collection.insertMany(employeeSeeds));
 
-		collection.drop();
+// 		console.log("Database seeded! :)");
+// 		client.close();
+// 	} catch (err) {
+// 		console.log(err.stack);
+// 	}
+// }
 
-		let employeeSeeds = [];
-
-		await axios
-			.get(
-				"https://randomuser.me/api/?results=50&nat=us&exc=gender,registered,nat,id,phone,login"
-			)
-			.then(function (response) {
-				// handle success
-				employeeSeeds = response.data.results;
-			})
-			.then(() => collection.insertMany(employeeSeeds));
-
-		console.log("Database seeded! :)");
-		client.close();
-	} catch (err) {
-		console.log(err.stack);
-	}
-}
-
-seedDB();
+// seedDB();
